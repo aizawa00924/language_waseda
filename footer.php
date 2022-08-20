31bc4ebda6adfb2473146c72b620f9539200ef9a
Shizuka Aizawa <40484201+aizawa00924@users.noreply.github.com>
2022-08-20 23:56:28 +0900

    初回アップ

--- footer.php
+++ footer.php
@@ -1,27 +1,19 @@
-<div class="footer">
-    <div class="footer_address">
-        <p class="txt_large">リブナビ株式会社</p>
-        <div class="footer_down">
-            <ul>
-                <li><a href="<?php echo esc_url(home_url('')); ?>">TOP</a></li>
-                <li><a href="<?php echo esc_url(home_url('')); ?>/service/">事業紹介</a></li>
-                <li><a href="<?php echo esc_url(home_url('')); ?>/company/">会社情報</a></li>
-
-                <li><a href="<?php echo esc_url(home_url('')); ?>/news/">お知らせ</a></li>
-                <li><a href="<?php echo esc_url(home_url('')); ?>/contact/">お問合せ</a></li>
-            </ul>
-
+<div class="footer_wrapper">
+        <div class="footer">
+            <div class="footer_school">
+            <div class="japanese">早稲田留学日本語教育センター</div>
+            <div class="english">Waseda Japanese Education Center</div>
+            <div class="address">〒162-0041 東京都新宿区早稲田鶴巻町251-45</div>
+            <div class="address">Tel: 03-3200-5560</div>
         </div>
+            
+        <div class="foot_group_introduce"><a href="/value-introduction/">グループ会社代表　価値観紹介</a></div>
     </div>
+            <div class="copyright">
+                <p class="copyright_txt pc">COPYRIGHT &copy; 2022 <?php bloginfo('name'); ?> AllRIGHTS RESERVED</p>
+                <p class="copyright_txt sp">COPYRIGHT &copy; 2022 <?php bloginfo('name'); ?> <br>AllRIGHTS RESERVED</p>
+            </div>
+        </div>
     
-    
-    
-    
-    <div class="copyright">
-        <p class="copyright_txt pc">COPYRIGHT &copy; 2022 <?php bloginfo('name'); ?> AllRIGHTS RESERVED</p>
-        <p class="copyright_txt sp">COPYRIGHT &copy; 2022 <?php bloginfo('name'); ?> <br>AllRIGHTS RESERVED</p>
-     </div>
-</div>
-
-<!-- /footer -->
-</body>
+    <!-- /footer -->
+    </body>
--- front-page.php
+++ front-page.php
@@ -12,19 +12,109 @@ Template Name: TOPページ
     <!--KV -->
     <div class="kv_wrapper" id="kv">
         <div class="container-video">
-            <div class="table-center">
-                <div>
-                  <p class="txt_small english">リブナビ株式会社 LIVENAVI Co., Ltd</p>
-                  <p class="pc"> 不動産の能力を最大限に活用し、お客様と共存共栄を目指します</p>
-                  <p class="sp"> 不動産の能力を最大限に活用し、<br>お客様と共存共栄を目指します</p>
-                   <p class="txt_small"></p>
-                </div>
-            </div>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/kv_top_pc.png" alt="リブナビ株式会社" class="pc">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/kv_top_sp.png" alt="リブナビ株式会社" class="sp">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/kv_top_pc.png" alt="早稲田留学日本語教育センター" class="pc">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/kv_top_sp.png" alt="早稲田留学日本語教育センター" class="sp">
         </div>
     </div>
     <!--KV -->
+        <!--about_wrapper start -->
+        <section class="consultup-service-section" id="service-section">
+                    <div class="consultup-heading">
+                      <h3 class="consultup-heading-inner">特長</h3>
+                      <p>Features</p>
+                    </div>
+                <!-- /Section Title -->
+                    <div class="row">
+                      <div class="col-sm-3 service-one">
+                          <div class="consultup-service-inner">
+                                <div class="ser-icon"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/top_icon01.png"></div>
+                                <h3>交通便利</h3>
+                                 <p>早稲田大学から<br>徒歩300m</p>
+                                 <a href="/introduce/" class="btn btn-theme">アクセス</a>	                
+                            </div>
+                      </div>
+                      <div class="col-sm-3 service-two">
+                          <div class="consultup-service-inner">
+                                <div class="ser-icon"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/top_icon02.png"></div>
+                                    <h3>サポート充実</h3>
+                                    <p>一対一<br>進学/就職指導</p>
+                                    <a href="/time/" class="btn btn-theme">授業内容</a>	
+                                </div>
+                            </div>
+                      <div class="col-sm-3 service-three">
+                          <div class="consultup-service-inner">
+                              <div class="ser-icon"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/top_icon03.png"></div>
+                                <h3>グローバル</h3>
+                                <p>多彩な<br>文化交流</p>
+                                 <a href="/schedule/" class="btn btn-theme">スケジュール</a>	
+                            </div>
+                      </div>
+                </div>
+        </section>
+        <!--about_wrapper fin -->
+        <!--consultup-callout start -->
+        <section class="consultup-callout" id="consultup-callout">
+             <div class="consultup-heading">
+                <h3 class="consultup-heading-inner">あなたの夢の実現に挑戦します</h3>
+              </div>
+              <div class="top_column_showwindow">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pic08_03.jpg" class="small">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pic08_02.jpg" class="small">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pic08_01.jpg" class="small">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pic08_04.jpg" class="small">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pic08_05.jpg" class="small">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pic08_06.jpg" class="small">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pic08_07.jpg" class="small">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pic08_08.jpg" class="small">
+                </div>
+        </section>
+        <!--consultup-callout fin -->
+        <!--consultup-portfolio start -->
+        <section class="consultup-portfolio" id="consultup-portfolio">
+            <div class="consultup-heading">
+                <h3 class="consultup-heading-inner">先生紹介</h3>
+            </div>
+            <div class="top_pro">
+                    <!--portfolio-->
+                    <div class="consultup-portfolio-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/teacher-1.jpg" alt="" title="米澤 文彦">
+                    </div>
+                    <!--/portfolio-->
+                    <div class="top_pro_wrapper">
+                        <div class="top_pro_title">学校長：米澤 文彦</div>
+                        <div class="top_pro_main">大学ではドイツ近現代文学、大学院では音楽理論、音楽史等を専攻した変わり種(教育学修士)。<br>専門学校の日本語科で留学生に日本語を教えると同時に日本語教師養成講座講師、大学での非常勤講師も務める。現在は日本語学校教務主任を務めている。<br>“学生と共に学び、共に成長する”をモットーに日々、多くの学生の指導に当たっている。</div>
+                        <div class="top_pro_message">
+                            <div class="title">ご挨拶</div>
+                            <div class="contents">チャレンジャーとして常に世界と向き合い、若者の未来に深い関心を寄せてきました。皆さんの夢の実現の為に力となること、これは早稲田留学日本語教育センターの存在理由であり伝統であると言えます。<br>
+                                ところで、皆さんの活躍の舞台である21世紀は20世紀とは違い、より複雑で一層グローバル化が浸透してゆく世界です。このような世界で夢を実現するためには、まずは外国語・異文化理解・IT技能という3つの分野で「基礎体力」をしっかりと養う必要があります。
+                                さらには、複雑な出来事の背後にある核心を把握する賢さと、世界が抱える問題について考えぬく力を鍛え上げることも、皆さんの人生をより高くより遠くへ導くうえで欠かせない大切なポイントです。<br>
+                                皆さんの夢を夢で終わらせないために、ぜひチャレンジし続ける勇気、挫けることの無い忍耐力、そして最後までやり抜く実行力を発揮してください。<br>
+                                本校は、夢の実現の為にやって来られる皆さんの真剣なまなざしと情熱に対して、全力でお応えすることをお約束します。
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </section>
+            <!--consultup-portfolio fin -->
+            <!--consultup-banner start -->
+        <section class="consultup-banner" id="consultup-banner">
+            <div class="top_banner pc">
+                <a href="/teacher_sen/">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top_banner001-scaled.jpg">
+                </a>
+            </div>
+            <div class="top_banner sp">
+                <a href="/teacher_sen/">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top_banner001_sp.jpg">
+                </a>
+            </div>
+        </section>
+            <!--consultup-banner fin -->
+    
+
+
+
+
     <div class="about_wrapper in_wrapper" id="top_about">
         <div class="top_title">
             <div class="english">About Us</div>
